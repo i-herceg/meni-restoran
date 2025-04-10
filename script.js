76150668bfe6317a11602c3642728fcd6a600fca
i-herceg <ivana.herceg25@gmail.com>
2025-04-10 19:18:08 +0200

    Nova verzija

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ document.addEventListener('DOMContentLoaded', function() {
         nav.classList.toggle('active');
     });
 });
+// Funkcija za otvaranje lightboxa
+function openLightbox(imgElement) {
+    var lightbox = document.getElementById('lightbox');
+    var lightboxImg = document.getElementById('lightbox-img');
+    lightbox.style.display = 'flex'; // Prikazivanje lightboxa
+    lightboxImg.src = imgElement.src; // Postavljanje izvora slike na kliknutu sliku
+  }
+  
+  // Funkcija za zatvaranje lightboxa
+  function closeLightbox() {
+    var lightbox = document.getElementById('lightbox');
+    lightbox.style.display = 'none'; // Sakrivanje lightboxa
+  }
+  
